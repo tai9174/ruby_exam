24be5b1f421fd8aa4501a204fb91a9ce3003fb87
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-05-24 10:40:27 +0900

    kadai

--- jan2.rb
+++ jan2.rb
@@ -1,29 +1,30 @@
-def hand
-    # プレイヤーにじゃんけんの手を選択させる文章を表示させます。
-    puts "数字を入力してください。"
-    puts "0:グー, 1:チョキ, 2:パー"
-    # 変数「input_hand」にプレイヤーの入力値を代入します。
-    # ヒント：getsメソッド1について調べてみましょう。
-    input_hand = gets.chomp
+def hand# プレイヤーにじゃんけんの手を選択させる文章を表示させます。
+
+  puts "数字を入力してください。"
+  puts "0:グー, 1:チョキ, 2:パー"
+  # 変数「input_hand」にプレイヤーの入力値を代入します。
+  # ヒント：getsメソッドについて調べてみましょう。
+   input_hand =gets.chomp
     # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
-    while true
-      # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
-      if input_hand == "0" || input_hand == "1" ||  input_hand == "2"
+  while true
+    # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
+    if input_hand == "0" ||input_hand == "1"|| input_hand == "2"
       # ヒント：include?メソッドについて調べてみましょう。
-        # 「input_hand」をそのまま返す。
-        return input_hand.to_i
-        # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
-      # else それ以外の場合
-      else
-        puts "0〜2の数字を入力してください。"
-        puts "0:グー, 1:チョキ, 2:パー"
-        # プレイヤーに「0〜2」を入力させる文章を表示させる。
-        # puts "0〜2の数字を入力してください。"
-        # puts "0:グー, 1:チョキ, 2:パー"
-        # 変数「input_hand」にプレイヤーの入力値を代入します。
-      # end if文のend
-      input_hand = gets.chomp
-      end
+    # 「input_hand」をそのまま返す。
+    return input_hand.to_i
+    # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
+    # else それ以外の場合
+      # プレイヤーに「0〜2」を入力させる文章を表示させる。
+    else
+      puts "0〜2の数字を入力してください"
+      puts "0:グー, 1:チョキ, 2:パー"
+      # puts "0〜2の数字を入力してください。"
+      # puts "0:グー, 1:チョキ, 2:パー"
+　　　　  # 変数「input_hand」にプレイヤーの入力値を代入します。
+     input_hand =gets.chomp
+    # end if文のend
     end
+    #end whileの分のend
   end
-  puts hand
+end
+puts hand
--- janken.rb
+++ janken.rb
@@ -1,39 +1,38 @@
-class Player# プレイヤー(自分)に「0~2」を入力させるロジックを書きます。
-  def hand# プレイヤーにじゃんけんの手を選択させる文章を表示させます。
 
+class Player
+def hand
+    # プレイヤーにじゃんけんの手を選択させる文章を表示させます。
     puts "数字を入力してください。"
     puts "0:グー, 1:チョキ, 2:パー"
     # 変数「input_hand」にプレイヤーの入力値を代入します。
-    # ヒント：getsメソッドについて調べてみましょう。
-     input_hand =gets.chomp
-      # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
+    # ヒント：getsメソッド1について調べてみましょう。
+    input_hand = gets.chomp
+    # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
     while true
       # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
-      if input_hand == "0" ||input_hand == "1"|| input_hand == "2"
-        # ヒント：include?メソッドについて調べてみましょう。
-      # 「input_hand」をそのまま返す。
-      return input_hand.to_i
-      # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
+      if input_hand == "0" || input_hand == "1" ||  input_hand == "2"
+      # ヒント：include?メソッドについて調べてみましょう。
+        # 「input_hand」をそのまま返す。
+        return input_hand.to_i
+        # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
       # else それ以外の場合
-        # プレイヤーに「0〜2」を入力させる文章を表示させる。
       else
-        puts "0〜2の数字を入力してください"
+        puts "0〜2の数字を入力してください。"
         puts "0:グー, 1:チョキ, 2:パー"
+        # プレイヤーに「0〜2」を入力させる文章を表示させる。
         # puts "0〜2の数字を入力してください。"
         # puts "0:グー, 1:チョキ, 2:パー"
-　　　　  # 変数「input_hand」にプレイヤーの入力値を代入します。
-       input_hand =gets.chomp
+        # 変数「input_hand」にプレイヤーの入力値を代入します。
       # end if文のend
+      input_hand = gets.chomp
       end
-      #end whileの分のend
     end
   end
 end
-
 # 相手が「0~2」の値をランダムに生成するロジックを書きます。
 class Enemy
   def hand
-    enemy_hand rand(0..2)# グー、チョキ、パーの値をランダムに取得する。
+    rand(0..2)# グー、チョキ、パーの値をランダムに取得する。
   end
 end
 # プレイヤー(自分)が入力した「0~2」と、敵がランダムで生成した「0~2」をじゃんけんをさせて、その結果をコンソール上に出力するロジックを書きます。
@@ -45,27 +44,28 @@ class Janken
     puts "相手の手は#{janken[enemy_hand]}です。"
     # Playerクラスの戻り値とEnemyクラスの戻り値からじゃんけんするロジックを作成します。
     # Playerクラスの戻り値(player_hand)とEnemyクラスの戻り値(enemy_hand)の値が同じだった場合
-     player_hand == enemy_hand
+     if player_hand == enemy_hand
       # 「あいこ」を出力します。
       puts "あいこ"
       #「true」を返してじゃんけんを繰り返し実行させます。
       # ヒント：「return」を使って戻り値を返すことができます。しかし、Rubyでは戻り値を返す場合、「return」を省略するのが一般的です。
-      return true
+      true
     elsif # もしも下記の組み合わせだった場合
       # (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
        (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
       puts "あなたの勝ちです"
       #「あなたの勝ちです」を出力します。
       #「false」を返してじゃんけんを終了させます。
-      return false
+       false
     else
       puts"あなたの負けです"
       #「あなたの負けです」を出力します。
       #「false」を返してじゃんけんを終了させます。
-      return false
+      false
     end
   end
 end
+
 # じゃんけんゲームを実行するロジックを書きます。
 class GameStart
   # selfを使用することで、GameStartをインスタンス化することなく、クラス名を使ってjankenponメソッドを呼び出せます。
@@ -77,6 +77,7 @@ class GameStart
     # 変数「janken」にJankenをインスタンス化したものを代入します。
     janken = Janken.new
     # 変数「next_game」に「true」を代入しましょう。
+    next_game = true
     # 「next_game」が「false」だと繰り返し処理を終了し、「true」だと繰り返し処理を継続します。
     while next_game
       # 変数「next_game」にじゃんけんを実行して返ってきた値(戻り値)を代入します。
